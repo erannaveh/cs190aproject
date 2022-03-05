3f468b7bc1b4b579d12b0efdaed4dee27ee78673
zacharyfriedland <zacharyfriedland@ucsb.edu>
2022-03-04 19:02:25 -0800

    changes added on branch

--- generate_data.py
+++ generate_data.py
@@ -96,8 +96,27 @@ def generate_one_long(n):
     x = 0
 
 def generate_money_increase_log(n):
-    # TODO
-    x = 0
+    new_n = int(n)
+    due_date = set()
+    time = []
+    profit = []
+
+    for i in range(new_n):
+        t = random.randint(60,900)
+        time.append(t)
+        profit.append(math.log(100+t) * random.uniform(.8, 1.2))
+
+    sum_time = sum(time)
+    i = 0
+    while len(due_date) < n:
+        d = random.randint(0, sum_time)
+        while d < time[i]:
+            d = random.randint(0, sum_time)
+        if d not in due_date:
+            due_date.add(d)
+            i += 1
+
+    return list(zip(time, profit, due_date))
 
 def generate_money_increase_sqrt(n):
     # TODO
